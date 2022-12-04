256816941f9b57297f464db26b85bca66b6bb73d
EnricoCaprioglio <ec627@sussex.ac.uk>
2022-12-04 22:19:40 +0000

    added Kuramoto.jl functions

--- src/Kuramodel.jl
+++ src/Kuramodel.jl
@@ -4,7 +4,9 @@ module Kuramodel
 export greet_Kuramodel
 export greet
 export randθ # this is to test how to add dependencies to a package
+export Kuramoto_step
 
 # where are these functions from?
 include("ExampleFunction.jl")
+include("Kuramoto.jl")
 end
