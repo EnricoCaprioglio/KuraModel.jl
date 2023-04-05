af167047b3191c14a96ea47c9fc5aa60359d1a1e
EnricoCaprioglio <ec627@sussex.ac.uk>
2023-04-06 00:26:52 +0100

    testing main fun for HPC

---
+++ Examples/Julia_main_fun.jl
@@ -0,0 +1,13 @@
+using Kuramodel
+
+# to call some function from the terminal, in order to sepcify the argument
+# which could be some environment variable from the job description
+# we can use the following:
+
+# julia -L file.jl -e 'main(some,args)'
+
+function main(arg)
+    println("This is the input to the main function: $arg")
+end
+
+greet()
