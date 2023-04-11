169984393391b8d07608f241993b25fd87ff4689
EnricoCaprioglio <ec627@sussex.ac.uk>
2023-04-11 20:26:50 +0100

    testing HPC paths

--- Examples/Julia_main_fun.jl
+++ Examples/Julia_main_fun.jl
@@ -1,29 +1,33 @@
 using Kuramodel
 using Plots
+using JDL2
 
 # to call some function from the terminal, in order to sepcify the argument
 # which could be some environment variable from the job description
 # we can use the following:
 
-# julia -L file.jl -e 'main(some,args)'
-
 function main(arg)
     println("This is the input to the main function: $arg")
 end
 
+# julia -L file.jl -e 'main(some,args)'
+# this imitates python's main function and call from the terminal.
+# However, in Julia we can just call all the functions in a file like this:
+
+# julia file.jl arg1 arg2 ...
+
 # println("Before greet()")
-
 # greet()
-
 # println("After greet()")
 
-# some calculations:
+# some random calculations:
 a = zeros(2,2)
 b = randn(5)
 c = "some string"
 
-println("Very nice calculations!\n")
-println("ARGS is the global variable for the arguments: $(ARGS)\n")
-println("While PROGRAM_FILE is the global variable of the name of the script: $(PROGRAM_FILE)")
+println("- ARGS is the global variable for the arguments: $(ARGS)\n")
+println("- While PROGRAM_FILE is the global variable of the name of 
+    the script: $(PROGRAM_FILE)\n"
+    )
 
-plot(b)
+println(pwd())
