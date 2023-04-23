25cc2d06375c54751b3aa647fc981965b88a0174
EnricoCaprioglio <ec627@sussex.ac.uk>
2023-04-23 20:59:05 +0100

    array job test

--- Examples/Julia_main_fun.jl
+++ Examples/Julia_main_fun.jl
@@ -1,5 +1,4 @@
 using Kuramodel
-using Plots
 using JLD2
 using Random
 
@@ -7,17 +6,17 @@ using Random
 # which could be some environment variable from the job description
 # we can use the following:
 
-function main(arg)
-    println("This is the input to the main function: $arg")
-end
+# function main(arg)
+#     println("This is the input to the main function: $arg")
+# end
 
 # julia -L file.jl -e 'main(some,args)'
 # this imitates python's main function and call from the terminal.
 # However, in Julia we can just call all the functions in a file like this:
-
 # julia file.jl arg1 arg2 ...
 
 println("- ARGS is the global variable for the arguments: $(ARGS)\n")
+
 println("- While PROGRAM_FILE is the global variable of the name of 
     the script: $(PROGRAM_FILE)\n"
     )
@@ -26,14 +25,13 @@ println("This is the current working directory: ", pwd())
 
 # set path
 store_data_path = "/mnt/nfs2/inf/ec627/src/Kuramodel/Examples/DataTest/"
-filename1 = "randomData1.jld2"
-filename2 = "randomData2.jld2"
+filenameroot = "randomData"
+arrayname = "$(ARGS)"
+extension = ".jld2"
 
 # create some random reproducible data
 Random.seed!(123)
-a = "$(ARGS)"
-b = randn(10)
+a = $(ARGS)
 
 # save test object
-save_object(store_data_path * filename1, a)
-save_object(store_data_path * filename2, b)
+save_object(store_data_path * filenameroot * arrayname * extension, a)
