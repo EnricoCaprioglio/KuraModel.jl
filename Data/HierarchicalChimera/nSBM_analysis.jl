4d06a6f7b229bd6bba1e5ccded7eadb5672944c0
EnricoCaprioglio <ec627@sussex.ac.uk>
2024-01-19 00:40:28 +0000

    update analysis

--- Data/HierarchicalChimera/nSBM_analysis.jl
+++ Data/HierarchicalChimera/nSBM_analysis.jl
@@ -4,11 +4,6 @@ using Random
 using JLD2
 using LinearAlgebra
 
-# set relevant paths
-folderpath = "/mnt/lustre/scratch/inf/ec627/data/HierarchicalChimera/paper_data/raw_data/"
-cd(folderpath)
-filenames = readdir()[1:end]
-
 # each file contains: results and params
 # results = θs
 # params = Dict(
@@ -29,18 +24,25 @@ filenames = readdir()[1:end]
 # β ∈ (0.05)
 
 # select params of interest
-seed_range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-β_range = 0.05; H_range = 0.45; k_range = 85.3
+seed_range = parse(Int, ARGS[1])
+β_range = 0.05; k_range = 85.3
+H_range = collect(0.00:0.01:1.00)
+
+# set relevant paths
+folderpath = "/mnt/lustre/scratch/inf/ec627/data/HierarchicalChimera/paper_data/raw_data/k_" * string(k_range) * "/"
+cd(folderpath)
+filenames = readdir()[1:end]
 
 # Define a regular expression pattern to match the numbers
 pattern = r"seed_(\d+)_beta_([\d.]+)_H_([\d.]+)_k_([\d.]+)\.jld2"
 
-# store data
-means_pop_1 = []; means_pop_2 = []
-stds_pop_1 = []; stds_pop_2 = []
-means_modules_pop_1 = []; means_modules_pop_2 = []
-var_modules_pop_1 = []; var_modules_pop_2 = []
-Hs = []
+# store data (each entry corresponds to a value of H, I added this since files are not ordered)
+no_data = length(H_range)
+means_pop_1 = zeros(no_data); means_pop_2 = zeros(no_data)
+stds_pop_1 = zeros(no_data); stds_pop_2 = zeros(no_data)
+means_modules_pop_1 = zeros(no_data); means_modules_pop_2 = zeros(no_data)
+var_modules_pop_1 = zeros(no_data); var_modules_pop_2 = zeros(no_data)
+Hs = zeros(no_data)
 
 for filename in filenames
     # Match the pattern in the filename
@@ -56,10 +58,10 @@ for filename in filenames
         k = parse(Float64, match_result[4])
 
         if seed in seed_range && β == β_range && k == k_range
-            # && H == H_range
 
-            # add to list of H studies
+            # add to list of H studied
             append!(Hs, H)
+            id = findfirst(x -> x == H, H_range)
 
             # load data
             θs, params = load_object(folderpath * filename)
@@ -68,10 +70,10 @@ for filename in filenames
 
             # layer 2 analysis (populations)
             pop_1_end = round(Integer, N/2)
-            append!(means_pop_1, mean(macro_op(θs[relax+1:end, 1:pop_1_end])))
-            append!(means_pop_2, mean(macro_op(θs[relax+1:end, pop_1_end+1:end])))
-            append!(stds_pop_1, std(macro_op(θs[relax+1:end, 1:pop_1_end])))
-            append!(stds_pop_2, std(macro_op(θs[relax+1:end, pop_1_end+1:end])))
+            means_pop_1[id] = mean(macro_op(θs[relax+1:end, 1:pop_1_end]))
+            means_pop_2[id] = mean(macro_op(θs[relax+1:end, pop_1_end+1:end]))
+            stds_pop_1[id] = std(macro_op(θs[relax+1:end, 1:pop_1_end]))
+            stds_pop_2[id] = std(macro_op(θs[relax+1:end, pop_1_end+1:end]))
 
             # layer 1 analysis (modules)
             modules_KOP_means = zeros(B[1]) # store mean KOP each module
@@ -85,10 +87,10 @@ for filename in filenames
 
             modules_pop_1_end = Integer(B[1]/2)
             # get average of all modules in a single population
-            append!(means_modules_pop_1, mean(modules_KOP_means[1:modules_pop_1_end]))
-            append!(means_modules_pop_2, mean(modules_KOP_means[modules_pop_1_end+1:end]))
-            append!(var_modules_pop_1, mean(modules_KOP_std[1:modules_pop_1_end])^2)
-            append!(var_modules_pop_2, mean(modules_KOP_std[modules_pop_1_end+1:end])^2)
+            means_modules_pop_1[id] = mean(modules_KOP_means[1:modules_pop_1_end])
+            means_modules_pop_2[id] = mean(modules_KOP_means[modules_pop_1_end+1:end])
+            var_modules_pop_1[id] = mean(modules_KOP_std[1:modules_pop_1_end])^2
+            var_modules_pop_2[id] = mean(modules_KOP_std[modules_pop_1_end+1:end])^2
         end
 
         # Print or store the extracted values
